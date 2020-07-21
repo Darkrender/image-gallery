cd2d31d619d7eeef19fd41013249c11e268643fe
Darkrender <jimmiegisclair@gmail.com>
2020-07-21 15:10:41 -0500

    Add lightbox effect to Gallery images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import Gallery from './components/gallery/gallery.component'
+import SimpleReactLightbox from 'simple-react-lightbox'
 
 function App() {
   return (
     <div className="App">
-      <Gallery />
+      <SimpleReactLightbox>
+        <Gallery />
+      </SimpleReactLightbox>
     </div>
   )
 }
--- src/components/gallery/gallery.component.jsx
+++ src/components/gallery/gallery.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './gallery.styles.scss'
 import useImageSearch from '../../hooks/useImageSearch'
+import { SRLWrapper } from 'simple-react-lightbox'
 
 const Gallery = () => {
   const [pageNumber] = useState(1)
@@ -10,15 +11,17 @@ const Gallery = () => {
     <div className="gallery">
       <h2 className="title">Image Gallery</h2>
 
-      <div className="images">
-        {images.map((item, index) => {
-          return (
-            <img key={index} src={item.urls.regular} alt={item.description} />
-          )
-        })}
-        {loading && <div>Loading...</div>}
-        {error && <div>Error</div>}
-      </div>
+      <SRLWrapper>
+        <div className="images">
+          {images.map((item, index) => {
+            return (
+              <img key={index} src={item.urls.regular} alt={item.description} />
+            )
+          })}
+          {loading && <div>Loading...</div>}
+          {error && <div>Error</div>}
+        </div>
+      </SRLWrapper>
     </div>
   )
 }
