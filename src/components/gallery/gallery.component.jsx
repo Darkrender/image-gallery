46151bb397fd3382097b214f5e3e3d29c66d1f89
Darkrender <jimmiegisclair@gmail.com>
2020-07-21 14:59:54 -0500

    Create responsive grid of photos

--- src/components/gallery/gallery.component.jsx
+++ src/components/gallery/gallery.component.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './gallery.styles.scss'
+import useImageSearch from '../../hooks/useImageSearch'
 
 const Gallery = () => {
-  return <div className="gallery"></div>
+  const [pageNumber] = useState(1)
+  const { images, loading, error } = useImageSearch(pageNumber)
+
+  return (
+    <div className="gallery">
+      <h2 className="title">Image Gallery</h2>
+
+      <div className="images">
+        {images.map((item, index) => {
+          return (
+            <img key={index} src={item.urls.regular} alt={item.description} />
+          )
+        })}
+        {loading && <div>Loading...</div>}
+        {error && <div>Error</div>}
+      </div>
+    </div>
+  )
 }
 
 export default Gallery
